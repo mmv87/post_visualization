73efe48e49768aa652c886441af2034479d0c22d
mithun_vmk <mithun.kct@gmail.com>
2026-03-30 20:33:52 -0400

    add

--- post_similarity.py
+++ post_similarity.py
@@ -16,27 +16,27 @@ token_ids=tokenizer(vocab,return_tensors='pt',add_special_tokens=False,padding=T
 
 ##check to get the name
 model=AutoModelForCausalLM.from_pretrained(modelpath,local_files_only=True)
+
 """for name, param in model.named_parameters():
-    print(f"name:{name}, parameter:{param.shape})"""
-    
-    ###to chechthe linear layers
-"""for name, module in model.named_modules():
+    print(f"name:{name}, parameter:{param.shape}")"""
+
+for name, module in model.named_modules():
     if isinstance(module, nn.Linear):
         print(f"Found a Linear Layer: {name}")
     else:
-        print(f"a module: {name},{type(module)}")"""
-""""       
+        print(f"a module: {name},{type(module)}")
+###to chechthe linear layers
+       
+"""    
 embed_layer=model.get_input_embeddings()
 lm_head = model.get_output_embeddings()
-
-print(type(embed_layer.weight),type(lm_head.weight))"""
-"""
+print(type(embed_layer.weight),type(lm_head.weight))
 for name,params in embed_layer.named_parameters():
     if params.requires_grad==True:
         print(params.shape)
     else:
         pass"""
-    
+"""
 labels=[tokenizer.decode(t) for t in token_ids[0]]
 ##load the embedding_npy
 embed_1=np.load("./stage_2_input_embed_1.npy")
@@ -49,7 +49,5 @@ plt.xticks(range(len(labels)), labels, rotation=45)
 plt.yticks(range(len(labels)), labels)
 
 plt.title("Pairwise Token Similarity")
-plt.show()
+plt.show()"""
 ##print(sim.shape)
-
-
--- test_post.py
+++ test_post.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 ##from TS_encoder import PatchTSTEncoder
 from  transformers import AutoModelForCausalLM,AutoTokenizer
-from ts_dataloader import ts_textual,collate_func
+##from ts_dataloader import ts_textual,collate_func
 import os
 import sys
 import numpy as np
@@ -20,27 +20,34 @@ from modules.ts_encoder import llm_projection"""
 device ='cuda' if torch.cuda.is_available() else 'cpu'
 
 ##location of llm_base model
+local_model_path="D:/hf_cache/hub/models--microsoft--Phi-4-mini-reasoning/snapshots/0e3b1e2d02ee478a3743abe3f629e9c0cb722e0a"
+
 model_name="/home/mmk/projects/def-zonata/mmk/hf_cache/hub/models--microsoft--Phi-4-mini-reasoning/snapshots/7a8c4e2e81eae20a606d811f475d7dc316dd916a"
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 os.environ["HF_HUB_OFFLINE"] = "1"
-model = AutoModelForCausalLM.from_pretrained(model_name,local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained(local_model_path,local_files_only=True)
+tokenizer=AutoTokenizer.from_pretrained(local_model_path,local_files_only=True)
+
 ##expanded tokenizer path
-tokenizer_path =os.path.join(os.environ["SLURM_TMPDIR"],'llm_tokenizer')
-tokenizer_modified =AutoTokenizer.from_pretrained(tokenizer_path)
+"""tokenizer_path =os.path.join(os.environ["SLURM_TMPDIR"],'llm_tokenizer')
+tokenizer_modified =AutoTokenizer.from_pretrained(tokenizer_path)"""
 
 model_dtype=next(model.parameters()).dtype
-model.resize_token_embeddings(len(tokenizer_modified))
+##model.resize_token_embeddings(len(tokenizer_modified))
+special_token_dict={'pad_token':"<|pad|>","additional_special_tokens":['<ts>','<ts/>']}
+tokenizer.add_special_tokens(special_token_dict)
+model.resize_token_embeddings(len(tokenizer))
 
 ## to expand the tokenizer to add the special tokens <ts> <ts/>
 """special_token_dict={'pad_token':"<|pad|>","additional_special_tokens":['<ts>','<ts/>']}
 tokenizer.add_special_tokens(special_token_dict)"""
 ##dataset fetching
 import json
-_json_file = os.path.join(os.environ["SLURM_TMPDIR"],"processed_dataset.jsonl")
+###_json_file = os.path.join(os.environ["SLURM_TMPDIR"],"processed_dataset.jsonl")
 
 ###datapipeline
-dataset=ts_textual(128,128,tokenizer_modified,_json_file,device=device)
-dataloader=DataLoader(dataset,batch_size=1,shuffle=True,collate_fn=lambda b:collate_func(b,tokenizer=tokenizer_modified))
+"""dataset=ts_textual(128,128,tokenizer_modified,_json_file,device=device)
+dataloader=DataLoader(dataset,batch_size=1,shuffle=True,collate_fn=lambda b:collate_func(b,tokenizer=tokenizer_modified))"""
 """
 dataset= ts_multimodal_text(128,128,_json_file,tokenizer,device=device,model_dtype=None)
 dataloader=DataLoader(dataset,batch_size=1,shuffle=True,collate_fn=lambda b:collate_func(b,tokenizer=tokenizer,device=device))"""
@@ -49,10 +56,8 @@ dataloader=DataLoader(dataset,batch_size=1,shuffle=True,collate_fn=lambda b:coll
 peft_config = LoraConfig(
             r=16, lora_alpha=32,
             target_modules=["o_proj",'qkv_proj','gate_up_proj','down_proj'],
-            modules_to_save=["embed_tokens"],lora_dropout=0.1, # important for Stage-2  as to keep th ties
+            modules_to_save=["embed_tokens","lm_head"],lora_dropout=0.1, # important for Stage-2  as to keep th ties
             task_type="CAUSAL_LM",ensure_weight_tying=True)
 
-
 peft_model=get_peft_model(model,peft_config)
-
-print(model.print_trainable_parameters())
+print(peft_model.print_trainable_parameters())
